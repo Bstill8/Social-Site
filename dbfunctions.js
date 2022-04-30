1be90fdb51640191323a30a5be56bd7138740ad0
Brendan Stillwell <stillwellbrendan@gmail.com>
2022-04-30 09:35:33 -0600

    created function for deleting users

--- dbfunctions.js
+++ dbfunctions.js
@@ -3,11 +3,12 @@ const db = require('./db')
 function createUser(conn, name, userName, password){
     return conn.query(`INSERT INTO Users (Name, UserName, Password) VALUES(\"${name}\", \"${userName}\", \"${password}\")`);
 }
-function deleteUSer(conn, userName){
+function deleteUser(conn, userName){
     return conn.query(`DELETE FROM Users WHERE UserName = "${userName}"`)
 }
 
 
 module.exports = {
     createUser,
+    deleteUser
 }
--- dbfunctions.test.js
+++ dbfunctions.test.js
@@ -11,12 +11,18 @@ test('Is connection valid',async() => {
 
     expect(Object.keys(await conn.query("show tables")).at(-1)).toBe("meta")
 })
-test('Test createUser', async() => {
+test('Test createUser/deleteUser', async() => {
     let response = await func.createUser(conn, "testName", "testUser", "testPwd")
-    console.log(response)
+
     expect(response.affectedRows).toEqual(1)
     expect(response.warningStatus).toEqual(0)
+    let read = await conn.query("SELECT * FROM Users WHERE UserName = 'testUser'")
+    expect(read[0].UserName).toEqual('testUser')
 
+    let delResponse = await func.deleteUser(conn, "testUser")
+
+    expect(delResponse.affectedRows).toEqual(1)
+    expect(delResponse.warningStatus).toEqual(0)
     
 });
 
